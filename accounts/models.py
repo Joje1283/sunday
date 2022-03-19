c3abafa9f1734f262cf6945bdbd05dd154ce3539
Jaesik <joje1283@gmail.com>
2022-03-19 17:17:06 +0900

    유저 모델에 결재자 필드를 추가

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,13 @@
+from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    approver = models.ForeignKey(
+        verbose_name='결재자',
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
