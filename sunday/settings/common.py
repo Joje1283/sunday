b3f9ae22fe79c90a180ccc6e4ae1a370aded4645
Jaesik <joje1283@gmail.com>
2022-03-19 17:04:47 +0900

    Accounts 앱 생성 유저 모델 변경

--- sunday/settings/common.py
+++ sunday/settings/common.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    # Thired Apps
+    'rest_framework',
+    # Project Apps
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +125,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = "accounts.User"
