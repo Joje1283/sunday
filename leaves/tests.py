e128bf99bf00ac904a6bcb90c24971a326fa504d
Jaesik <joje1283@gmail.com>
2022-03-20 18:53:10 +0900

    코드 정리

--- leaves/tests.py
+++ leaves/tests.py
@@ -1,7 +1,7 @@
-from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-from .models import Use, Grant, Type
+from .models import Grant, Type
 
 User = get_user_model()
 
--- leaves/views.py
+++ leaves/views.py
@@ -1,13 +1,11 @@
-from django.contrib.auth import get_user_model
-
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.generics import CreateAPIView, GenericAPIView
+from rest_framework.generics import CreateAPIView
 
-from .serializers import UseSerializer, GrantSerializer
 from .models import Use
+from .serializers import UseSerializer, GrantSerializer
 
 
 class UseCreateView(APIView):
