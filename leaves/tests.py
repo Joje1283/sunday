4552434b8185c71b01261db96e39017918525a40
Jaesik <joje1283@gmail.com>
2022-03-20 19:47:04 +0900

    테스트 코드 메서드 정의

--- leaves/tests.py
+++ leaves/tests.py
@@ -87,13 +87,80 @@ class LeaveTestCase(TestCase):
         self.assertEqual(res.status_code, 201)
 
     def test_일반사용자가_남은_휴가를_조회한다(self):
-        pass
+        self.test_관리자가_일반사용자에게_휴가를_부여한다()
+        self.client.force_login(self.일반사용자1)
+        res = self.client.get(
+            path="/leaves/count/",
+        )
 
     def test_일반사용자가_올해_휴가_내역을_조회한다(self):
+        self.test_관리자가_일반사용자에게_휴가를_부여한다()
+        self.client.force_login(self.일반사용자1)
+        res = self.client.get(
+            path="/leaves/",
+            data={
+                "type": "all"
+            }
+        )
+
+    def test_일반사용자가_신청한_휴가를_취소한다(self):
         pass
 
     def test_일반사용자가_예약된_휴가를_조회한다(self):
+        self.test_관리자가_일반사용자에게_휴가를_부여한다()
+        self.client.force_login(self.일반사용자1)
+        res = self.client.get(
+            path="/leaves/",
+            data={
+                "type": "reservation"
+            }
+        )
+
+    def test_관리자가_요청된_휴가를_조회한다(self):
+        pass
+
+    def test_관리자가_요청한_휴가를_수락한다(self):
         pass
 
     def test_관리자가_모든_예약된_휴가를_조회한다(self):
-        pass
+        self.test_관리자가_일반사용자에게_휴가를_부여한다()
+
+        # 일반 사용자들이 휴가 사용
+        self.client.force_login(self.일반사용자1)
+        res = self.client.post(
+            path="/leaves/use/",
+            data={
+                "type": Type.ANNURE,
+                "start_date": "2022-01-02",
+                "end_date": "2022-01-03",
+                "start_date_time": "11:00:00"
+            }
+        )
+        self.client.force_login(self.일반사용자1)
+        res = self.client.post(
+            path="/leaves/use/",
+            data={
+                "type": Type.SPECIAL,
+                "start_date": "2022-01-04",
+                "end_date": "2022-01-10",
+                "start_date_time": "11:00:00"
+            }
+        )
+        self.client.force_login(self.일반사용자2)
+        res = self.client.post(
+            path="/leaves/use/",
+            data={
+                "type": Type.ANNURE,
+                "start_date": "2022-01-02",
+                "end_date": "2022-01-03",
+                "start_date_time": "11:00:00"
+            }
+        )
+
+        self.client.force_login(self.관리자)
+        res = self.client.get(
+            path="/leaves/",
+            data={
+                "type": "admin"
+            }
+        )
